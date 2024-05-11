a448a810571cbfc988aa4746e5b766d744dce395
BryanS1989 <bryanstevenal@gmail.om>
2024-05-11 17:08:20 +0200

    Deleting state from the store

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -13,4 +13,11 @@ import TodoList from './TodoList.vue';
 </template>
 
 <style scoped>
+.todo-app {
+    padding: 5vh;
+    min-height: 100vh;
+    width: 50vw;
+    margin: 0 auto;
+    background-color: #f7f9fc;
+}
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -11,25 +11,48 @@ const store = useTodoListStore();
 */
 const { todoList } = storeToRefs(store); // storeToRefs lets todoList keep reactivity:
 
-const { toggleCompleted } = store;
+const { toggleCompleted, deleteTodo } = store;
 </script>
 
 <template>
     <div v-for="todo in todoList" :key="todo.id" class="item">
         <div class="content">
             <span :class="{ completed: todo.completed }">{{ todo.item }}</span> 
-            <span @click.stop="toggleCompleted(todo.id)" class="check-todo">&#10004;</span>
+            <span>
+                <span @click.stop="toggleCompleted(todo.id)" class="check-todo">&#10004;</span>
+                <span @click="deleteTodo(todo.id)" class="delete-todo">&#10006;</span>
+            </span>
         </div>
     </div>
 </template>
 
 <style scoped>
+span {
+    margin: 0 10px;
+    cursor: pointer;
+}
+.item {
+    display: flex;
+    justify-content: center;
+}
+.content {
+    display: flex;
+    font-size: 1.5em;
+    justify-content: space-between;
+    width: 80vw;
+    padding: 5px;
+}
 .completed {
     text-decoration: line-through;
 }
 .check-todo {
     cursor: pointer;
-    padding-left: 1rem;
-    color:#00ff00
+    color:#00ff00;
+    font-size: 1.5rem;
+}
+.delete-todo {
+    cursor: pointer;
+    color:#ff0000;
+    font-size: 1.5rem;
 }
 </style>
